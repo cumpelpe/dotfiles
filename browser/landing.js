0270601db047ddad4d45759b18923c415215e180
Petr <cumpelpe@gmail.com>
2020-11-14 21:36:03 +0100

    Commit to date

--- browser/landing.js
+++ browser/landing.js
@@ -19,33 +19,33 @@ String.prototype.capitalize = function () {
 };
 let now = new Date();
 const dateCode = `${now.getFullYear()}${now.getDate()}${now.getMonth()}`;
-const dateCodeOffset = function(offset)  {
-	let now = new Date();
-	now.setDate(now.getDate() + offset);
-	return `${now.getFullYear()}${now.getDate()}${now.getMonth()}`;
+const dateCodeOffset = function (offset) {
+  let now = new Date();
+  now.setDate(now.getDate() + offset);
+  return `${now.getFullYear()}${now.getDate()}${now.getMonth()}`;
 }
 const links = [
-  { name: "Youtube",	url: "https://www.youtube.com" },
-  { name: "Courses",	url: "https://courses.fit.cvut.cz" },
-  { name: "Fittable",	url: "https://timetable.fit.cvut.cz/new/" },
-  { name: "Marast",	url: "https://marast.fit.cvut.cz/" },
-  { name: "Progtest",	url: "https://progtest.fit.cvut.cz/" },
+  { name: "Youtube",  url: "https://www.youtube.com"            },
+  { name: "Courses",  url: "https://courses.fit.cvut.cz"        },
+  { name: "Fittable", url: "https://timetable.fit.cvut.cz/new/" },
+  { name: "Marast",   url: "https://marast.fit.cvut.cz/"        },
+  { name: "Progtest", url: "https://progtest.fit.cvut.cz/"      },
 ];
 const DB = new Dexie("quote");
 DB.version(2).stores({
-  quote:	'date,txt,author,category',
-  corona:	'date,cases,todayCases,active,deaths,todayDeaths,recovered,todayRecovered',
+  quote: 'date,txt,author,category',
+  corona: 'date,cases,todayCases,active,deaths,todayDeaths,recovered,todayRecovered',
 });
 const getQuote = async () => {
   const now = new Date();
-  if ( await DB.quote.get(dateCode) ) return await DB.quote.get(dateCode);
+  if (await DB.quote.get(dateCode)) return await DB.quote.get(dateCode);
   console.log("NotInDB");
   const URL = "https://quotes.rest/qod?language=en";
   const data = (await fetch(URL).then((res) => res.json()))["contents"]["quotes"][0];
   const res = {
-    txt:	data["quote"],
-    category:	data["category"],
-    author:	data["author"],
+    txt: data["quote"],
+    category: data["category"],
+    author: data["author"],
   };
   DB.quote.put({ date: dateCode, ...res });
   return res;
@@ -60,14 +60,14 @@ const timer = document.querySelector("#realtime");
 const corona = document.querySelector("#corona");
 const linksDiv = document.querySelector("#links");
 
-const interval = setInterval(() => {
+const interval = window.setInterval(() => {
   const now = new Date();
   timer.innerText = `${now.getDate().toString().padStart(2, "0")}.${
-	  (now.getMonth() + 1).toString().padStart(2, "0")
-  }.${now.getFullYear()} ${now
-    .getHours()
-    .toString()
-    .padStart(2, "0")}:${now.getMinutes().toString().padStart(2, "0")}`;
+    (now.getMonth() + 1).toString().padStart(2, "0")
+    }.${now.getFullYear()} ${now
+      .getHours()
+      .toString()
+      .padStart(2, "0")}:${now.getMinutes().toString().padStart(2, "0")}`;
 }, 1000);
 
 const setupLinks = (links, linksDiv) => {
@@ -82,7 +82,7 @@ const setupLinks = (links, linksDiv) => {
 setupLinks(links, linksDiv);
 
 const getCoronaData = async (divInto) => {
-  class heading {
+  class Heading {
     constructor(index, name = "") {
       this._index = index;
       this._name = name;
@@ -92,7 +92,7 @@ const getCoronaData = async (divInto) => {
       return this._name;
     }
     set name(newName) {
-      this._name = newName;	
+      this._name = newName;
     }
     get index() {
       return this._index;
@@ -101,37 +101,37 @@ const getCoronaData = async (divInto) => {
 
   const URL = "https://corona-stats.online/cz?format=json";
 
-  const data = await fetch(URL).then((data) => data.json());
-  console.log(data);
+  const data = await fetch(URL).then(data => data.json());
   divInto.innerHTML = "";
-  const h 	= document.createElement("h2");
-  const table 	= document.createElement("table");
+  const h = document.createElement("h2");
+  const table = document.createElement("table");
   const tr = () => document.createElement("tr");
   const th = () => document.createElement("th");
   const td = () => document.createElement("td");
-  let putData = { date: dateCode }
-  const yesterdayCorona = await DB.corona.get(dateCodeOffset(-1));
+  let putData = { date: dateCode };
+  let yesterdayCorona = await DB.corona.get(dateCodeOffset(-1));
+  yesterdayCorona = yesterdayCorona === undefined ? "unknown" : yesterdayCorona;
   for (const i of [
-    new heading("active", "ActiveCases"),
-    new heading("todayCases", "today ΔCases"),
-    new heading("cases", "TotalCases"),
-    new heading("todayDeaths", "today ΔDeaths"),
-    new heading("deaths", "deathsTotal"),
-    new heading("todayRecovered", "today ΔRecovered"),
-    new heading("recovered", "recoveredTotal"),
+    new Heading("active",         "ActiveCases"     ),
+    new Heading("todayCases",     "today ΔCases"    ),
+    new Heading("cases",          "TotalCases"      ),
+    new Heading("todayDeaths",    "today ΔDeaths"   ),
+    new Heading("deaths",         "deathsTotal"     ),
+    new Heading("todayRecovered", "today ΔRecovered"),
+    new Heading("recovered",      "recoveredTotal"  ),
   ]) {
     const trow = tr();
     table.appendChild(trow);
     let txt;
-    if ( data["data"][0][i.index] == 0 ){
+    if (data["data"][0][i.index] == 0) {
       txt = yesterdayCorona[i.index];
-      i.name = i.name.replace( /today/i, 'yesterday' );
+      i.name = i.name.replace(/today/i, 'yesterday');
     }
     else
       txt = data["data"][0][i.index];
     putData[i.index] = data["data"][0][i.index];
     const tdata = td(),
-    thead = th();
+      thead = th();
     tdata.innerText = txt;
     thead.innerText = i.name.capitalize().splitCamel();
     trow.appendChild(thead);
