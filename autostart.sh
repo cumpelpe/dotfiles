4a64ca693fd581c692a2207a2ec23828a16cd2c8
Petr Čumpelík <cumpelpe@gmail.com>
2021-02-15 09:34:20 +0100

    Add config for picom

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
+
+## Autostart to be used with my xmonad config
 pa-applet &
 setxkbmap -model pc104 -layout us,cz  -option 'grp:alt_shift_toggle'
 picom -b
 $HOME/Pictures/Wallpapers/randomWallpaper.sh
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --alpha 0 --height 16 &
-#mpd 
